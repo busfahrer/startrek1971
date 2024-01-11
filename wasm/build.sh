d21b99824e0b22fb970f16d0f9032378353e0607
busfahrer <1844369+busfahrer@users.noreply.github.com>
2024-01-12 00:21:26 +0100

    add emcc line

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# Try the following line to activate the emscripten environment
+# source /etc/profile.d/emscripten.sh
 wget -c https://github.com/mame/xterm-pty/raw/main/emscripten-pty.js
 emcc -s ASYNCIFY --js-library=emscripten-pty.js -o st.mjs ../st.c
