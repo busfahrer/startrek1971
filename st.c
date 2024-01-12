acf2e3dc6f9ee48305e4fe3f8191f5ebd53eebae
busfahrer <1844369+busfahrer@users.noreply.github.com>
2024-01-12 21:10:28 +0100

    add header comment

--- st.c
+++ st.c
@@ -1,9 +1,27 @@
-/* Based on the 2008 C# port by Michael Birken: https://meatfighter.com/startrek1971/ */
+/*
+ * Copyright (C) 2024 busfahrer
+ *
+ * This port is based on the 2008 C# port by Michael Birken: https://meatfighter.com/startrek1971/
+ *
+ * Star Trek is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Star Trek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h> /* don't forget -lm */
 
-/* srand(0); */
 #ifdef __STDC_VERSION__
 #if __STDC_VERSION__ >= 199901L
 #define PRINT(...) printf(__VA_ARGS__); fflush(stdout);
